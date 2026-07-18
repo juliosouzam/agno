27f04bda345a1da6476e7b76749a4aa8c6d711ec
Júlio <julio.souzam@gmail.com>
2026-07-18 11:33:52 -0300

    feat: add AsyncOracleDb async database implementation

--- libs/agno/agno/db/oracle/__init__.py
+++ libs/agno/agno/db/oracle/__init__.py
@@ -1,5 +1,6 @@
 """Oracle database provider for Agno."""
 
+from agno.db.oracle.async_oracle import AsyncOracleDb
 from agno.db.oracle.oracle import OracleDb
 
-__all__ = ["OracleDb"]
+__all__ = ["OracleDb", "AsyncOracleDb"]
